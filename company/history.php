bfc938488b9024546a8e38e10d93f3a8d8f48f9f
Shakhov Evgeniy <79115839983@ya.ru>
2019-05-17 16:15:16 +0300

    ex1-25

--- company/history.php
+++ company/history.php
@@ -1,5 +1,6 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetPageProperty("color_head", "green");
 $APPLICATION->SetTitle("История");
 ?> 
 <table cellspacing="10"> 
